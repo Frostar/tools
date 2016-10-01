1964e95ece8849223ef57c3bbf89bfa81d98eabb
Jens Frost <post@j-frost.dk>
2016-10-01 15:13:37 +0200

    spotify_album_url: no more writing of current trackid to /tmp/

--- spotify_album_url.sh
+++ spotify_album_url.sh
@@ -11,7 +11,6 @@
 
 # Grep current track ID as argument
 TRACKID=$(echo $1 | cut -f3- -d':')
-echo $TRACKID > '/tmp/current_track.txt'
 
 # Curl Track Info, grep url for images, take the first one, cut url between 3th " and 4th "
 curl -s -X GET "https://api.spotify.com/v1/tracks/$TRACKID" | grep "\"url\" : \"https://i.scdn.co/image/" | head -1 | cut -d "\"" -f4 | cut -d "\"" -f1
