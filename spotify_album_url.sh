41c8d9afb184710a55cada3ac04997339423f22a
Jens Frost <jfl@lakeside.dk>
2017-05-31 10:05:57 +0200

    Spotify_album_url.sh updated to handle personal spotify token

--- spotify_album_url.sh
+++ spotify_album_url.sh
@@ -12,5 +12,8 @@
 # Grep current track ID as argument
 TRACKID=$(echo $1 | cut -f3- -d':')
 
-# Curl Track Info, grep url for images, take the first one, cut url between 3th " and 4th "
-curl -s -X GET "https://api.spotify.com/v1/tracks/$TRACKID" | grep "\"url\" : \"https://i.scdn.co/image/" | head -1 | cut -d "\"" -f4 | cut -d "\"" -f1
+# Get personal spotify token. Can generated here https://developer.spotify.com/web-api/console/get-track/
+TOKEN=$(cat ~/.spotify/token)
+
+# Curl Track Info with token, grep url for images, take the first one, cut url between 3th " and 4th "
+curl -s -X GET "https://api.spotify.com/v1/tracks/$TRACKID" -H "Accept: application/json" -H "Authorization: Bearer $TOKEN" | grep "\"url\" : \"https://i.scdn.co/image/" | head -1 | cut -d "\"" -f4 | cut -d "\"" -f1
